e49aa5f87cf37012d7df38710e232cea1a292eb9
Sam <hc.ngu01@gmail.com>
2021-01-14 14:53:20 +0800

    Test: array key on page 2 and onwards are now reset

--- tests/PaginationTest.php
+++ tests/PaginationTest.php
@@ -20,4 +20,25 @@ class PaginationTest extends TestCase
 
         $this->assertEquals($pageSize, $paginated->perPage());
     }
+
+    public function test_paginated_array_keys_are_reset_on_page2_and_onward()
+    {
+        $collection = collect([1,2,3,4,5,6,7,8]);
+
+        $pageSize = 4;
+
+        /** @var $request \Illuminate\Http\Request */
+        $request = $this->app['request'];
+
+        // supply page query param to request
+        $request->merge([
+            'page' => '2'
+        ]);
+
+        $paginated = CollectionPaginator::paginate($collection, $pageSize);
+
+        $this->assertSame([5, 6, 7, 8], array_values($paginated->all()) );
+        $this->assertSame([0, 1, 2, 3], array_keys($paginated->all()) );
+
+    }
 }
